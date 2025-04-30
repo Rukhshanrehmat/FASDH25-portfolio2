62e5e1176aaf98cbd47981ca26cd718b6a8a45e5
shabirkarim <shabir1023@gmail.com>
2025-04-30 15:30:29 +0500

    added changes

---
+++ Scripts/regex_script_ShabirKarim.py
@@ -0,0 +1,42 @@
+#import libraries
+
+
+# Load the gazetteer file from portfolio repo
+
+# Read all columns from the gazetteer
+
+#Build the regex patterns
+
+#define a function to build these regex
+
+
+
+
+
+#Load in data  - list of  people, folder articles
+
+#path to folder
+
+# list of people
+
+#list of files in folder
+
+#Initiate empty dictionary
+
+# loop through articles - in the list of files -
+    # and loop through people to get counts of people
+
+    #log the frequencies in a dictionary
+
+# convert to tsv format
+
+#create list with one item - person\tfreqeuncy\n
+
+#loop through the dictionary and convert to tsv - person\tfrqeunceos\n
+
+#Join list using \n
+
+#Export tsv 
+
+
+# tsv with frequencies of people in the all articles 
